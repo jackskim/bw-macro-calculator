bbad1a656688a0ff7e032070a0f020828f55049a
seongck <seongk@pm.me>
2019-06-27 19:28:32 -0700

    Remove extra variable for .nav-bottom

--- components/navigation.js
+++ components/navigation.js
@@ -15,6 +15,8 @@ class NavMenu {
     }
 }
 
+
+// Get the menu
 const navMenu = document.querySelector('.nav-bottom');
 new NavMenu(navMenu);
 
@@ -25,18 +27,15 @@ window.onscroll = function() {
   stickyNav();
 };
 
-// Get the navbar
-const navbar = document.querySelector('.nav-bottom');
-
 // Get the offset position of the navbar
-const sticky = navbar.offsetTop;
+const sticky = navMenu.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll
 // position.  Remove it when you leave the scroll position
 function stickyNav() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add('sticky');
+    navMenu.classList.add('sticky');
   } else {
-    navbar.classList.remove('sticky');
+    navMenu.classList.remove('sticky');
   }
 }
