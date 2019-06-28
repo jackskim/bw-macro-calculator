935cde14545d569038e13565a42adfa402bf74f3
seongck <seongk@pm.me>
2019-06-27 18:55:45 -0700

    Fix merge conflict

--- components/navigation.js
+++ components/navigation.js
@@ -1,3 +1,4 @@
+// MOBILE HAMBURGER MENU
 class NavMenu {
     constructor(navigation) {
         this.navigation = navigation;
@@ -16,3 +17,26 @@ class NavMenu {
 
 const navMenu = document.querySelector('.nav-bottom');
 new NavMenu(navMenu);
+
+
+// STICKY NAV
+// When the user scrolls, execute stickyNav
+window.onscroll = function() {
+  stickyNav();
+};
+
+// Get the navbar
+const navbar = document.querySelector('.nav-bottom');
+
+// Get the offset position of the navbar
+const sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll
+// position.  Remove it when you leave the scroll position
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');
+  }
+}
